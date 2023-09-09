89a4251e5cced2e586cbc059ca95eaa44fd65a0b
Dangis <dangiskazukauskas@gmail.com>
2023-09-09 22:16:15 +0300

    skills section using animation done

---
+++ components/skills.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { skillsData } from "@/lib/data";
+import { useSectionInView } from "@/lib/hooks";
+import { motion } from "framer-motion";
+
+export default function Skills() {
+  const { ref } = useSectionInView("Skills");
+
+  return (
+    <section
+      ref={ref}
+      id="skills"
+      className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40 mt-28"
+    >
+      <SectionHeading>My skills</SectionHeading>
+      <ul className="flex gap-2 flex-wrap justify-center text-gray-800 text-lg">
+        {skillsData.map((skill, index) => (
+          <motion.li
+            key={index}
+            variants={fadeInAnimationVariants}
+            initial="initial"
+            whileInView="animate"
+            viewport={{
+              once: true,
+            }}
+            custom={index}
+            className="bg-white border border-black/[0.1] rounded-xl px-5 py-3"
+          >
+            {skill}
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+const fadeInAnimationVariants = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: index * 0.05,
+    },
+  }),
+};

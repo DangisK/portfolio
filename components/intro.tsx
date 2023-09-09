3ef0701908beab4fb1595072e285dae2cc7edd75
Dangis <dangiskazukauskas@gmail.com>
2023-09-09 14:32:27 +0300

    Intro animations, functioning buttons

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,6 +3,10 @@
 import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { ImGithub } from "react-icons/im";
 
 export default function Intro() {
   return (
@@ -60,6 +64,55 @@ export default function Intro() {
         <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
       </motion.p>
+
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 
+        flex items-center gap-2 rounded-full outline-none focus:scale-110 
+        hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+        >
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full 
+        outline-none focus:scale-110 hover:scale-110 active:scale-105 transition
+        cursor-pointer border border-black/10"
+          href="/CV.pdf"
+          download
+        >
+          Download CV <HiDownload className="opacity-60 group-hover:translate-y-0.5 transition" />
+        </a>
+
+        <a
+          className="bg-white p-4 flex items-center gap-2 rounded-full text-gray-700
+        focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition
+        cursor-pointer border border-black/10"
+          href="https://www.linkedin.com/in/dangis-ka%C5%BEukauskas-83b0241a1/"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          className="bg-white p-4 flex items-center gap-2 rounded-full text-gray-700
+        focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition
+        cursor-pointer border border-black/10"
+          href="https://github.com/DangisK"
+          target="_blank"
+        >
+          <ImGithub />
+        </a>
+      </motion.div>
     </section>
   );
 }
